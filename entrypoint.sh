6e56ab2f394cc62e5a711de36db920ae47ef44e0
Qian Linfeng <thewawar@gmail.com>
2018-06-05 23:05:45 +0800

    Fix can not run with root issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Add local user(name=user) use USER_ID
-useradd --shell /bin/bash -u ${USER_ID} -o -c "" -d /opt user
-# Can use sudo without password
-echo "user ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+if [ "${USER_ID}" = "0" ] || [ "${USER_ID}" = "" ]; then
+    # Run with "root"
+    exec "$@"
+else
+    # Add local user(name=user) use USER_ID
+    useradd --shell /bin/bash -u "${USER_ID}" -o -c "" -d /opt user
+    # Can use sudo without password
+    echo "user ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
-exec /usr/bin/gosu user "$@"
+    exec /usr/bin/gosu user "$@"
+fi
+
