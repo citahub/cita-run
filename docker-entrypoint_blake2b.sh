e5eabfddcd297e18ae3f541549eed3c3c11ef6ce
jiangxianliang <jiangxianliang@cryptape.com>
2018-10-10 11:17:02 +0800

    Newly added sm2 and blake2b algorithms compile docker image

---
+++ docker-entrypoint_blake2b.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "start run cita"
+./scripts/create_cita_config.py create --nodes "127.0.0.1:4000,127.0.0.1:4001,127.0.0.1:4002,127.0.0.1:4003"
+./bin/cita setup test-chain/0
+./bin/cita setup test-chain/1
+./bin/cita setup test-chain/2
+./bin/cita setup test-chain/3
+nohup ./bin/cita start test-chain/0 &
+nohup ./bin/cita start test-chain/1 &
+nohup ./bin/cita start test-chain/2 &
+nohup ./bin/cita start test-chain/3 &
+
+/bin/bash -c "while true;do sleep 100;done"
