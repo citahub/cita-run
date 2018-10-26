a3fef95965aa8f02642e60cc95096a21f0e60b11
jiangxianliang <jiangxianliang@cryptape.com>
2018-10-26 14:13:27 +0800

    Change versionmanager.version ID 1 to 0

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "start run cita"
-./scripts/create_cita_config.py create --nodes "127.0.0.1:4000,127.0.0.1:4001,127.0.0.1:4002,127.0.0.1:4003" --contract_arguments "SysConfig.economicalModel=1"
+./scripts/create_cita_config.py create  --nodes "127.0.0.1:4000,127.0.0.1:4001,127.0.0.1:4002,127.0.0.1:4003" --contract_arguments "SysConfig.economicalModel=1" --contract_arguments VersionManager.version=0
 ./bin/cita setup test-chain/0
 ./bin/cita setup test-chain/1
 ./bin/cita setup test-chain/2
