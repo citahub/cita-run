68f5d9145724f635e03321f196657cc552413128
jiangxianliang <jiangxianliang@cryptape.com>
2018-11-26 15:52:01 +0800

    Modify protocol version to 0

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "start run cita"
-./scripts/create_cita_config.py create  --nodes "127.0.0.1:4000,127.0.0.1:4001,127.0.0.1:4002,127.0.0.1:4003" --super_admin 0x4b5ae4567ad5d9fb92bc9afd6a657e6fa13a2523 --contract_arguments "SysConfig.economicalModel=1" --contract_arguments VersionManager.version=0
+./scripts/create_cita_config.py create  --nodes "127.0.0.1:4000,127.0.0.1:4001,127.0.0.1:4002,127.0.0.1:4003" --super_admin 0x4b5ae4567ad5d9fb92bc9afd6a657e6fa13a2523 --contract_arguments "SysConfig.economicalModel=1"
 ./bin/cita setup test-chain/0
 ./bin/cita setup test-chain/1
 ./bin/cita setup test-chain/2
